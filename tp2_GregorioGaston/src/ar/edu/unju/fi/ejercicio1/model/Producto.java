6bca973646b548b61464173c4e43e7be409b39eb
GastonT800 <m.nick5164@gmail.com>
2024-04-22 20:55:50 -0300

    se la rama del ejercicio1 y se crearon los package para el ejercicio1

---
+++ tp2_GregorioGaston/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.ejercicio1.model;
+
+public class Producto {
+	//Se crean los atributos
+	private int codigo;
+	private String descripcion;
+	private double precioUnitario;
+
+}
